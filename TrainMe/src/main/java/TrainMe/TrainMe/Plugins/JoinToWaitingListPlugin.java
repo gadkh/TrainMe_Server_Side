46c03e2a9491c750bce29c06bc9ba4b605656bce
Golan Dor <golandor12@gmail.com>
2019-04-04 12:20:56 +0300

    Finished Plugin: JoinToWaitingList

--- TrainMe/src/main/java/TrainMe/TrainMe/Plugins/JoinToWaitingListPlugin.java
+++ TrainMe/src/main/java/TrainMe/TrainMe/Plugins/JoinToWaitingListPlugin.java
@@ -9,31 +9,29 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import TrainMe.TrainMe.FireBase.logic.IFireBase;
 import TrainMe.TrainMe.logic.entity.ActivityEntity;
-import TrainMe.TrainMe.logic.entity.CourseEntity;
 
 @Component
-public class RateCoursePlugin implements TrainMePlugins {
+public class JoinToWaitingListPlugin implements TrainMePlugins {
 
 	private ObjectMapper jackson;
 	private IFireBase firebaseService;
-	
+
 	@Autowired
-	public RateCoursePlugin(IFireBase firebaseService) {
+	public JoinToWaitingListPlugin(IFireBase firebaseService) {
 		this.firebaseService = firebaseService;
 		this.jackson = new ObjectMapper();
 	}
-	
+
 	@Override
 	public Object invokeAction(ActivityEntity activityEntity) {
-		RateCourse rateCourse=new RateCourse();
+		JoinToWaitingList joinToWaitingList = new JoinToWaitingList();
 		try {
-			rateCourse = this.jackson.readValue(activityEntity.getAttributesJson(), RateCourse.class);
-			this.firebaseService.rateCourse(rateCourse.getCourseId(), rateCourse.getUserId(), rateCourse.getRate());
-			return rateCourse;
+			joinToWaitingList = this.jackson.readValue(activityEntity.getAttributesJson(), JoinToWaitingList.class);
+			this.firebaseService.joinToWaitingList(joinToWaitingList.getCourseId(), joinToWaitingList.getUser());
+			return joinToWaitingList;
 		} catch (IOException e) {
-			throw new RuntimeException(e);		
-			}
+			throw new RuntimeException(e);
+		}
 	}
-	
 
 }
