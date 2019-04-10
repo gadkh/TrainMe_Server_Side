3d1e8e7f6c37a1a17fa5f4312d45dfd9156db3b8
Gadkh2 <gadkh2@gmail.com>
2019-04-10 10:55:00 +0300

    first commit

--- TrainMe/src/main/java/TrainMe/TrainMe/FireBase/logic/JPA/FireBaseMethods.java
+++ TrainMe/src/main/java/TrainMe/TrainMe/FireBase/logic/JPA/FireBaseMethods.java
@@ -269,7 +269,6 @@ public class FireBaseMethods implements IFireBase {
 		databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
 			@Override
 			public void onDataChange(DataSnapshot snapshot) {
-<<<<<<< HEAD
 				generalFlag = false;
 				if (snapshot.child("Courses").child(courseId).child("registered").child(userId).exists()) {
 					generalFlag = true;
@@ -277,7 +276,6 @@ public class FireBaseMethods implements IFireBase {
 					generalFlag = false;
 				}
 				countDownLatch.countDown();
-=======
 				generalFlag=false;
 				 if(snapshot.child("Courses").child(courseId).child("registered").child(userId).exists()){
 					 generalFlag=true;				
@@ -287,7 +285,6 @@ public class FireBaseMethods implements IFireBase {
 					 generalFlag=false;
 				 }
 				 countDownLatch.countDown();
->>>>>>> master
 			}
 
 			@Override
